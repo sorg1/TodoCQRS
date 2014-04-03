9180b0315c80e5a3e61f477dfa2e69f031406739
sorg1 <sorg1@mail.ru>
2014-04-03 21:08:38 +0300

    add Commands

---
+++ TodoCQRS.Domain/Todo/Events/Todo_RemovedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Paralect.Domain;
+
+namespace TodoCQRS.Domain.Todo.Events
+{
+    class Todo_RemovedEvent : Event
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ TodoCQRS.Domain/Todo/Events/Todo_UpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Paralect.Domain;
+
+namespace TodoCQRS.Domain.Todo.Events
+{
+    class Todo_UpdatedEvent : Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
